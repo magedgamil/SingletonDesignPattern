a3e140430969c8946fb53bafdc43bdb8b75dc38a
maged gamil <magedgamil200@gmail.com>
2024-10-24 00:52:23 +0300

    Add project files.

---
+++ SingletonDesignPattern/ConfigurationManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern;
+public class ConfigurationManager
+{
+    private static readonly Lazy<ConfigurationManager> _instance = new Lazy<ConfigurationManager>(() => new ConfigurationManager());
+
+    private ConfigurationManager()
+    {
+
+    }
+    public static ConfigurationManager Instance
+    {
+        get
+        {
+            return _instance.Value;
+        }
+
+    }
+    public string GetConfiguration(string key)
+    {
+        // Return configuration value based on key
+        return "SomeValue";
+    }
+}
---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,11 @@
+﻿using SingletonDesignPattern;
+
+var configManager1 = ConfigurationManager.Instance;
+var configManager2 = ConfigurationManager.Instance;
+if (configManager1 == configManager2)
+{
+    Console.WriteLine("Both instances are the same.");
+}
+string configValue = configManager1.GetConfiguration("SomeKey");
+Console.WriteLine("Configuration Value: " + configValue);
+Console.ReadLine();
